236248959bc5d73bb3bd695f5fa474d56bbaf2fa
Stefan Erwin Baumer <baumeredv@gmail.com>
2025-11-08 14:42:12 +0100

    ci(nix): add build flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = 
+    { nixpkgs, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          bun
+          typescript-language-server
+        ];
+      };
+    };
+}
